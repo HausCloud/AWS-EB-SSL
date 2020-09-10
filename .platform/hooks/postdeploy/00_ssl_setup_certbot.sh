ee9621767359d78506e203bdba72d254f1101963
HausCloud <hauscloud.me@gmail.com>
2020-09-10 23:07:34 +0000

    Refactor, add safeguards, add logging

--- .platform/hooks/postdeploy/00_ssl_setup_certbot.sh
+++ .platform/hooks/postdeploy/00_ssl_setup_certbot.sh
@@ -1,40 +1,98 @@
 #!/usr/bin/env bash
-# Bash script to attach to postdeploy hook for SSL
+# Postdeploy script for enabling SSL (single instance)
 # Compatible only with Amazon Linux 2 EC2 instances
 
+LOG_PATH=$(find /var/log/ -type f -iname 'eb-hooks.log')
+DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+CERTBOT_NAME='<certificate_name>'
+CERTBOT_EMAIL='<certificate_email>'
+# Multiple domain example: CERTBOT_DOMAINS='bort.com,www.bort.com,bort-env.eba-2kg3gsq2.us-east-2.elasticbeanstalk.com'
+CERTBOT_DOMAINS='<eb_url>'
+
+crontab_exists() {
+    crontab -u root -l 2>/dev/null | grep 'certbot -q renew' >/dev/null 2>/dev/null
+}
+
+log_and_exit() {
+    if [ -n "$LOG_PATH" ] && [ -n "$DATE" ]; then
+        echo "$DATE | $1" | tee -a "$LOG_PATH"
+    fi
+    exit 0
+}
+
+# Variable check
+
+if [ ! -n "$CERTBOT_NAME" ] || [ ! -n "$CERTBOT_EMAIL" ] || [ ! -n "$CERTBOT_DOMAINS" ]; then
+    log_and_exit 'INFO: Certbot and/or proxy server information is missing.'
+fi
+
 # Auto allow yes for all yum install
-# Suggestion: Remove after deployment
+# SUGGESTION: Remove after deployment
+
 if ! grep -q 'assumeyes=1' /etc/yum.conf; then
     echo 'assumeyes=1' | tee -a /etc/yum.conf
 fi
 
-# Increase size of string name for --domains
-if which nginx; then
-    http_string='^http\s*{$'
-    bucket_increase='http {\nserver_names_hash_bucket_size 300;\n'
-    sed -i "s/$http_string/$bucket_increase/g" /etc/nginx/nginx.conf
-fi
-
 # Install EPEL
 # Source: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/amazon-linux-ami-basics.html
+
 if ! yum list installed epel-release; then
     yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 fi
 
-if ! [ -x "$(command -v certbot)" ] && yum list installed epel-release; then
+# Install certbot
+
+if yum list installed epel-release && ! command -v certbot &>/dev/null; then
     yum install certbot python2-certbot-nginx
 fi
 
-if [ ! -z "${CERTBOT_CERT_NAME+x}" ] && [[ -n "$CERTBOT_CERT_NAME" ]] && [ ! -z "${CERTBOT_EMAIL+x}" ] && [[ -n "$CERTBOT_EMAIL" ]] && [ ! -z "${CERTBOT_DOMAIN_LIST+x}" ] && [[ -n "$CERTBOT_DOMAIN_LIST" ]] && [ -x "$(command -v certbot)" ]; then
-    certbot --nginx --redirect --debug --cert-name "$CERTBOT_CERT_NAME" -m "$CERTBOT_EMAIL" --domains "$CERTBOT_DOMAIN_LIST" --agree-tos --no-eff-email --keep-until-expiring --non-interactive
+
+HTTP_STRING='^http\s*{$'
+NAME_LIMIT='http {\nserver_names_hash_bucket_size 192;\n'
+
+# Prevent replace if not clean sample app
+
+if ! grep -Fxq "$NAME_LIMIT" /etc/nginx/nginx.conf; then
+    # Increase size of string name for --domains (for default EB configs)
+    
+    if ! sed -i "s/$HTTP_STRING/$NAME_LIMIT/g" /etc/nginx/nginx.conf; then
+        log_and_exit 'ERROR: Changing server name limit failed'
+    fi
 fi
 
-crontab_exists() {
-    crontab -l 2>/dev/null | grep 'certbot -q renew' >/dev/null 2>/dev/null
-}
-if ! crontab_exists ; then
+# Prevent certificate installation if not clean sample app
+
+CERT_REGEX="Certificate Name:\s+$CERTBOT_NAME"
+
+if certbot certificates | grep -Ew "$CERT_REGEX"; then
+    log_and_exit 'INFO: Certificate already installed.'
+fi
+
+# Set up certificates
+
+if command -v certbot &>/dev/null; then
+    if nginx -t; then
+        certbot --nginx --cert-name "$CERTBOT_NAME" -m "$CERTBOT_EMAIL" --domains "$CERTBOT_DOMAINS" --redirect --agree-tos --no-eff-email --keep-until-expiring --non-interactive
+    else
+        log_and_exit 'ERROR: Nginx configuration is invalid.'
+    fi
+else
+    log_and_exit 'ERROR: Certbot installation may have failed.'
+fi
+
+# Create cron task (attempt) to renew certificate every 29 days
+
+if ! crontab_exists; then
     systemctl start crond
     systemctl enable crond
-    line="0 0 */29 * * certbot -q renew; systemctl reload nginx.service"
-    (crontab -u root -l; echo "$line" ) | crontab -u root -
+    
+    LINE="0 0 */29 * * certbot -q renew; service nginx reload; service nginx restart"
+    
+    (
+        crontab -u root -l
+        echo "$LINE"
+    ) | crontab -u root -
 fi
+
+log_and_exit 'SUCCESS: Script ran successfully.'
